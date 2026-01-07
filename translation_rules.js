1527e51c80a7a738ebbe7a3bd2d843373410b1f5
tierraofficial <tierraofficial@users.noreply.github.com>
2026-01-07 15:14:31 +0800

    update windPrefix translation rule

--- translation_rules.js
+++ translation_rules.js
@@ -9,7 +9,7 @@ const TRANSLATION_RULES = {
     // 公共字典库
     "dictionaries": {
         "digits": {
-            "zh": { "0": "洞", "1": "幺", "2": "两", "3": "三", "4": "四", "5": "五", "6": "六", "7": "拐", "8": "八", "9": "九" },
+            "zh": { "0": "冻", "1": "幺", "2": "两", "3": "三", "4": "四", "5": "五", "6": "六", "7": "拐", "8": "八", "9": "九" },
             "en": { "0": "Zero", "1": "One", "2": "Two", "3": "Three", "4": "Four", "5": "Five", "6": "Six", "7": "Seven", "8": "Eight", "9": "Niner" }
         },
         "airlines": {
@@ -24,6 +24,10 @@ const TRANSLATION_RULES = {
             "zh": { "A": "Alpha", "B": "Bravo", "C": "Charlie", "D": "Delta", "E": "Echo", "F": "Foxtrot", "G": "Golf", "H": "Hotel", "I": "India", "J": "Juliet", "K": "Kilo", "L": "Lima", "M": "Mike", "N": "November", "O": "Oscar", "P": "Papa", "Q": "Quebec", "R": "Romeo", "S": "Sierra", "T": "Tango", "U": "Uniform", "V": "Victor", "W": "Whiskey", "X": "X-ray", "Y": "Yankee", "Z": "Zulu" },
             "en": { "A": "Alpha", "B": "Bravo", "C": "Charlie", "D": "Delta", "E": "Echo", "F": "Foxtrot", "G": "Golf", "H": "Hotel", "I": "India", "J": "Juliet", "K": "Kilo", "L": "Lima", "M": "Mike", "N": "November", "O": "Oscar", "P": "Papa", "Q": "Quebec", "R": "Romeo", "S": "Sierra", "T": "Tango", "U": "Uniform", "V": "Victor", "W": "Whiskey", "X": "X-ray", "Y": "Yankee", "Z": "Zulu" }
         },
+        "windPrefix": {
+            "zh": { "地面风": "地面风-" },
+            "en": { "Wind": "Wind-" }
+        },
         "runwayPrefix": {
             "zh": { "RWY": "跑道", "RUNWAY": "跑道" },
             "en": { "RWY": "runway ", "RUNWAY": "runway ", "Runway": "runway " }
@@ -35,9 +39,10 @@ const TRANSLATION_RULES = {
         "AircraftCallSign": { "useDictionaries": ["airlines", "digits"] },
         "Constant": { "useDictionaries": ["underscore", "digits"] },
         "TaxiRoute": { "useDictionaries": ["runwayPrefix", "phonetic", "digits"] },
-        "WindDynamic": { "useDictionaries": ["digits"] },
+        "WindDynamic": { "useDictionaries": ["windPrefix", "digits"] },
         "Runway": { "useDictionaries": ["runwayPrefix", "phonetic", "digits"] },
         "Spot": { "useDictionaries": ["digits"] },
+        "HoldingPoint": { "useDictionaries": ["digits", "phonetic"] },
         "TurnDirection": { "useDictionaries": [] },
         "PushbackHeading": { "useDictionaries": [] },
         "NavigationPoint": { "useDictionaries": [] }
